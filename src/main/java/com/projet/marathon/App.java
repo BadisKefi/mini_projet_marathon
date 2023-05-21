03358a2cb22bfd0215d2a6d82d1b9bb3a9f0455d
badiskefi <badiskefi@gmail.com>
2023-05-22 00:34:49 +0100

    commit 5.1 with adding marathon gestion and better routting

--- src/main/java/com/projet/marathon/App.java
+++ src/main/java/com/projet/marathon/App.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/part3/gestion_marathon.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/part2/login.fxml"));
         Scene scene = new Scene(parent);
         primaryStage.setTitle("Marathon Ensemble");
         primaryStage.setScene(scene);
